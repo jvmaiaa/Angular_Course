0bb13c938f2f9e5abc4d6b424f30aa7b9fb0534f
jvmaiaa <joaovictormaia482@gmail.com>
2025-08-23 04:49:38 -0300

    ✨[FEAT]: Cria lógica de acesso aleatório a usuário no componente UseComponente.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { UserComponent } from './user/user.component';
 
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
