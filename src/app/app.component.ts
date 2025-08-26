42d9fc9cfbdd9aa13e170e0e214984820dfa16d0
jvmaiaa <joaovictormaia482@gmail.com>
2025-08-26 04:18:40 -0300

    ✨[FEAT]: Adiciona o componente Tasks e integra sua exibição no componente principal, atualizando a lógica de seleção de usuário para emitir o nome.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './task/tasks.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS
+  users = DUMMY_USERS;
+  selectedUser: string = '';
 
-  onSelectUser(id: string) {
-    console.log("Id from user " + id);
+  // get selectedUser() {
+  //   return this.users.find((user) => user.id == this.selectedUserId)!;
+  // }
+
+  onSelectUser(name: string) {
+    this.selectedUser = name;
   }
 }
