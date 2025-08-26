cbcfcd7864a86735e670b08b3ebce054fffdee7d
jvmaiaa <joaovictormaia482@gmail.com>
2025-08-25 23:04:45 -0300

    ✨[FEAT]: Atualiza o componente User para corrigir a gestão de propriedades e melhorar a lógica de seleção de usuário.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
@@ -9,19 +9,17 @@ import { Component, computed, input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  imagePath = computed(() => {
-    return 'assets/users/' + this.avatar();
-  })
+  @Output() select = new EventEmitter();
 
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }
